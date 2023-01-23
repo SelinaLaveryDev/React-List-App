89809f950a1142ace4f2e269c4c5ede0f576c0e5
Selina Lavery <selina118@hotmail.com>
2023-01-23 15:36:44 +0000

    Unordered list added

--- restaurant-app/src/Components/List.js
+++ restaurant-app/src/Components/List.js
@@ -16,6 +16,13 @@ class List extends Component {
 					/>
 					<button type="">Search</button>
 					<button>Clear</button>
+					<ul>
+						<li>Restaurant 1</li>
+						<li>Restaurant 2</li>
+						<li>Restaurant 3</li>
+						<li>Restaurant 4</li>
+						<li>Restaurant 5</li>
+					</ul>
 				</form>
 			</>
 		);
