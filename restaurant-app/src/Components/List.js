ea3fbe8bd85b159b6240d7bbda56192618977637
Selina Lavery <selina118@hotmail.com>
2023-01-23 14:57:11 +0000

    Initial react app set-up, List.js component created.

---
+++ restaurant-app/src/Components/List.js
@@ -0,0 +1,15 @@
+import React, { Component } from "react";
+
+class List extends Component {
+	// state = {}
+
+	render() {
+		return (
+			<>
+				<h1>Weekend Away</h1>
+			</>
+		);
+	}
+}
+
+export default List;
