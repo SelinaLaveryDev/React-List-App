eb394950f429c30e00e11676c08233c3b764168c
Selina Lavery <selina118@hotmail.com>
2023-01-24 15:03:18 +0000

    added restaurantsToDisplay to this state and updated setState with filtered restaurants - following Usmans video lesson. I now need to add the API data.

--- restaurant-app/src/Components/App.js
+++ restaurant-app/src/Components/App.js
@@ -9,6 +9,7 @@ class App extends Component {
 
 		this.state = {
 			searchValue: "",
+			restaurantsToDisplay: this.props.restaurantList,
 			// restaurantList: this.props.restaurantList,
 		};
 	}
@@ -21,20 +22,35 @@ class App extends Component {
 		});
 	};
 
-	// handleSearch = (e) => {
-	// 	e.preventDefault();
-	// 	console.log("handling search...");
-	// 	this.setState({
-	// 		city:
-	// 	})
-	// };
+	handleSearch = (e) => {
+		e.preventDefault();
+		// console.log(e.target.value);
+		const textValue = e.target.value;
+
+		//Take the current value of the text box
+		//Fitler the restaurantList Array into a new array
+
+		const filteredRestaurants = this.props.restaurantList.filter(
+			(restaurant) => {
+				return restaurant.toLowerCase().includes(textValue.toLowerCase());
+			}
+		);
+
+		this.setState({
+			searchValue: textValue,
+			restaurantsToDisplay: filteredRestaurants,
+		});
+	};
 
 	render() {
 		return (
 			<>
 				<h1>Weekend Away</h1>
-				<Search value={this.state.searchValue} />
-				<ListContainer restaurantList={this.props.restaurantList} />
+				<Search
+					value={this.state.searchValue}
+					onChange={this.handleSearch}
+				/>
+				<ListContainer restaurantList={this.state.restaurantsToDisplay} />
 				<button onClick={this.clearList}>Clear</button>
 			</>
 		);
--- restaurant-app/src/Components/Search.js
+++ restaurant-app/src/Components/Search.js
@@ -5,9 +5,10 @@ class Search extends Component {
 		return (
 			<>
 				<form>
-					<label>What city are you visiting?</label>
+					<label>Where are you dining out this weekend?</label>
 					<input
 						type="text"
+						placeholder="Enter city here..."
 						value={this.props.value}
 						onChange={this.props.onChange}
 					/>
