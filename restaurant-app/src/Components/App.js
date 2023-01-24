b66276db3a1c75ebd071a8017b8c4d20cabf2c17
Selina Lavery <selina118@hotmail.com>
2023-01-24 17:17:21 +0000

    Added API fetch in componentWillMount

--- restaurant-app/src/Components/App.js
+++ restaurant-app/src/Components/App.js
@@ -15,6 +15,32 @@ class App extends Component {
 		};
 	}
 
+	componentDidMount() {
+		const apiKey = process.env.REACT_APP_X_RapidAPI_Key;
+
+		const options = {
+			method: "GET",
+			headers: {
+				"X-RapidAPI-Key": process.env.REACT_APP_X_RapidAPI_Key,
+				"X-RapidAPI-Host": "travel-advisor.p.rapidapi.com",
+			},
+		};
+
+		fetch(
+			"https://travel-advisor.p.rapidapi.com/restaurants/list?location_id=293919&restaurant_tagcategory=10591&restaurant_tagcategory_standalone=10591&currency=USD&lunit=km&limit=30&open_now=false&lang=en_US",
+			options
+		)
+			.then((response) => response.json())
+			.then((response) => {
+				console.log(response);
+				// this.setState({
+				// 	restaurantData: response.data,
+				// 	restaurantsToDisplay: response.data,
+				// });
+			})
+			.catch((err) => console.error(err));
+	}
+
 	clearList = (e) => {
 		e.preventDefault();
 		console.log("Clearing List...");
