2d0fe8c56a7fa2b68f92ff768b66330968d31cf9
Selina Lavery <selina118@hotmail.com>
2023-01-26 09:29:45 +0000

    Adding New Restaurant Form component as part of MVP

---
+++ restaurant-app/src/Components/AddRestaurantForm.js
@@ -0,0 +1,13 @@
+import React, { Component } from "react";
+
+class AddRestaurantForm extends Component {
+	render() {
+		return (
+			<>
+				<h1>Add Restaurant Form</h1>
+			</>
+		);
+	}
+}
+
+export default AddRestaurantForm;
